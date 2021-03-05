9ead1f535d1ccc44aec29e603676505dee5695c3
grobothero <hechangxin@gmail.com>
2021-03-05 22:34:36 +0100

    finish HW1

--- HW1/pca_normal.py
+++ HW1/pca_normal.py
@@ -1,9 +1,10 @@
-# 实现PCA分析和法向量计算，并加载数据集中的文件进行验证
+#implement the calculation of PCA and the norm
 
 import open3d as o3d 
 import os
 import numpy as np
 from pyntcloud import PyntCloud
+import matplotlib.pyplot as plt
 
 # 功能：计算PCA的函数
 # 输入：
@@ -13,11 +14,19 @@ from pyntcloud import PyntCloud
 # 输出：
 #     eigenvalues：特征值
 #     eigenvectors：特征向量
+
 def PCA(data, correlation=False, sort=True):
     # 作业1
     # 屏蔽开始
 
-
+    data =np.array(data)
+    if (correlation==False):
+        data_mean = np.mean(data,axis=0)
+        data = data - data_mean
+    n= np.shape(data)[0]
+    c_mat = data.transpose()@data/(n-1)  #correlation or covariance
+    #np.cov(c_mat)
+    eigenvalues, eigenvectors = np.linalg.eig(c_mat)
     # 屏蔽结束
 
     if sort:
@@ -29,16 +38,16 @@ def PCA(data, correlation=False, sort=True):
 
 
 def main():
-    # 指定点云路径
-    # cat_index = 10 # 物体编号，范围是0-39，即对应数据集中40个物体
-    # root_dir = '/Users/renqian/cloud_lesson/ModelNet40/ply_data_points' # 数据集路径
-    # cat = os.listdir(root_dir)
-    # filename = os.path.join(root_dir, cat[cat_index],'train', cat[cat_index]+'_0001.ply') # 默认使用第一个点云
+        # 指定点云路径
+    cat_index = 5# 物体编号，范围是0-39，即对应数据集中40个物体/home/chahe/Documents/shenlan_pointcloud/ModelNet40/airplane
+    root_dir = '/home/chahe/project/shenlan_pointcloud/ModelNet40' # 数据集路径
+    cat = os.listdir(root_dir)
+    filename = os.path.join(root_dir, cat[cat_index],'train', cat[cat_index]+'_0001.off') # 默认使用第一个点云
 
     # 加载原始点云
-    point_cloud_pynt = PyntCloud.from_file("/Users/renqian/Downloads/program/cloud_data/11.ply")
+    point_cloud_pynt = PyntCloud.from_file(filename)
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
-    # o3d.visualization.draw_geometries([point_cloud_o3d]) # 显示原始点云
+    #o3d.visualization.draw_geometries([point_cloud_o3d]) # 显示原始点云
 
     # 从点云中获取点，只对点进行处理
     points = point_cloud_pynt.points
@@ -46,25 +55,31 @@ def main():
 
     # 用PCA分析点云主方向
     w, v = PCA(points)
-    point_cloud_vector = v[:, 2] #点云主方向对应的向量
+    point_cloud_vector = np.array(v[:,:2]) #点云主方向对应的向量,取前两列
+    points_pca = np.array(points)@point_cloud_vector
+    print(np.shape(points_pca))
+    #points_pca = points_pca.tolist()
+    #print(points_pca)
+    plt.title("the first two principles of the points")
+    plt.plot(points_pca[:,0],points_pca[:,1])
+    plt.show()
     print('the main orientation of this pointcloud is: ', point_cloud_vector)
-    # TODO: 此处只显示了点云，还没有显示PCA
-    # o3d.visualization.draw_geometries([point_cloud_o3d])
-    
     # 循环计算每个点的法向量
     pcd_tree = o3d.geometry.KDTreeFlann(point_cloud_o3d)
-    normals = []
-    # 作业2
-    # 屏蔽开始
-
-    # 由于最近邻搜索是第二章的内容，所以此处允许直接调用open3d中的函数
-
-    # 屏蔽结束
+    points =np.array(points)
+    n=points.shape[0]
+    normals = np.zeros((n,3))
+    for i in range(n):
+        _,idxs,_ =pcd_tree.search_knn_vector_3d(points[i],16)
+        points_tmp =points[idxs]
+        w, v = PCA(points_tmp)
+        v_tmp =v[2] 
+        normals[i,:]=v[2]
+          
     normals = np.array(normals, dtype=np.float64)
-    # TODO: 此处把法向量存放在了normals中
+    #print(np.shape(normals))
     point_cloud_o3d.normals = o3d.utility.Vector3dVector(normals)
     o3d.visualization.draw_geometries([point_cloud_o3d])
-
 
 if __name__ == '__main__':
     main()
--- HW1/voxel_filter.py
+++ HW1/voxel_filter.py
@@ -1,3 +1,4 @@
+# -*- coding: UTF-8 -*-
 # 实现voxel滤波，并加载数据集中的文件进行验证
 
 import open3d as o3d 
@@ -13,12 +14,42 @@ def voxel_filter(point_cloud, leaf_size):
     filtered_points = []
     # 作业3
     # 屏蔽开始
+    r=leaf_size
+    points = np.array(point_cloud)
+    print("original point shape ",np.shape(points))
+    x_max=np.max(points[:,0])
+    x_min=np.min(points[:,0])
+    y_max=np.max(points[:,1])
+    y_min=np.min(points[:,1])
+    z_max=np.max(points[:,2])
+    z_min=np.min(points[:,2])
+    d_x =np.ceil((x_max-x_min)/r)
+    d_y =np.ceil((y_max-y_min)/r)
+    d_z =np.ceil((z_max-z_min)/r)
+    h_x = np.floor((points[:,0]-x_min)/r)
+    h_y = np.floor((points[:,1]-y_min)/r)
+    h_z = np.floor((points[:,2]-z_min)/r)
+    h = h_x + h_y*d_x +h_z*d_z*d_y
+    h=h.reshape(-1,1)
+    points_id =np.hstack((points,h))
+    points_id =points_id[points_id[:,3].argsort()]
+    max_idx =np.max(points_id[:,3])
+    min_idx=np.min(points_id[:,3])
+    data_size =np.shape(points_id)[0]
+    data_dict = {}
+    for i in range(data_size):
+        if points_id[i,3] not in data_dict.keys():
+            data_dict[points_id[i,3]]=points_id[i,:3]
+            #use the first element
 
- 
+    val_array=np.array([])
+    for ele in data_dict.values():
+        val_array=np.append(ele,val_array)
+    val_array=val_array.reshape([-1,3]) 
     # 屏蔽结束
 
     # 把点云格式改成array，并对外返回
-    filtered_points = np.array(filtered_points, dtype=np.float64)
+    filtered_points = np.array(val_array, dtype=np.float64)
     return filtered_points
 
 def main():
@@ -31,14 +62,18 @@ def main():
 
     # 加载自己的点云文件
     file_name = "/Users/renqian/Downloads/program/cloud_data/11.ply"
-    point_cloud_pynt = PyntCloud.from_file(file_name)
+    cat_index=5
+    root_dir = '/home/chahe/project/shenlan_pointcloud/ModelNet40' # 数据集路径
+    cat = os.listdir(root_dir)
+    filename = os.path.join(root_dir, cat[cat_index],'train', cat[cat_index]+'_0001.off') # 默认使用第一个点云
+    point_cloud_pynt = PyntCloud.from_file(filename)
 
     # 转成open3d能识别的格式
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
-    # o3d.visualization.draw_geometries([point_cloud_o3d]) # 显示原始点云
+    #o3d.visualization.draw_geometries([point_cloud_o3d]) # 显示原始点云
 
     # 调用voxel滤波函数，实现滤波
-    filtered_cloud = voxel_filter(point_cloud_pynt.points, 100.0)
+    filtered_cloud = voxel_filter(point_cloud_pynt.points, 3.0)
     point_cloud_o3d.points = o3d.utility.Vector3dVector(filtered_cloud)
     # 显示滤波后的点云
     o3d.visualization.draw_geometries([point_cloud_o3d])
