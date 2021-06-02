dca64265bac47a2159b4b714bccc85f5ff5b66a2
grobothero <hechangxin@gmail.com>
2021-06-02 21:23:36 +0200

    finish init of icp

--- HW9/registration.py
+++ HW9/registration.py
@@ -26,12 +26,6 @@ def VisualizePcd(pcd):
     o3d.visualization.draw_geometries([pcd])
 
 
-def get_matched_id(src_idx,source_fpfh,target_fpfh):
-    source_fpfh_sample= source_fpfh[:,[src_idx]]
-    feature_diff = np.linalg.norm(target_fpfh - source_fpfh_sample,axis=0)
-    match_id = np.argmin(feature_diff)
-    return match_id,feature_diff[match_id]
-
 def procrustes_transformation(A, B):
     """ Procrustes was a rogue smith and bandit from Attica who attacked people 
     by stretching them or cutting off their legs, so as to force them 
@@ -63,40 +57,31 @@ def procrustes_transformation(A, B):
     t= np.expand_dims(B_mean - R@A_mean,1)
     
     return R,t
+
+def getMatchesFromFeature(src_features,tgt_features):
+    """The matches are formed based on the feature descriptors
+
+    Parameters
+    ----------
+    src_features : [numpy.array m*N]
+        [description]
+        source feature
+    tgt_features : [numpy.array  m*N]
+        [description]
+        target features
+
+    Returns
+    -------
+    [list]
+        [description]
+        A list of matched indexs tuple
         
-# %%
-if __name__ == "__main__":
-    
-    #load data
-    filename1 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+"0.bin"
-    filename2 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+"456.bin"
-    source_pcd = getCuttedPcd(filename1)
-    target_pcd = getCuttedPcd(filename2)
-    #test_pcd =io.read_point_cloud_bin(filename1)
-    #source_pcd.points
-    # %%
-     #VisualizePcd(source_pcd)
-    #VisualizePcd(target_pcd)
-    src_data = read_oxford_bin(filename1)[:3]
-    tgt_data = read_oxford_bin(filename2)[:3]
-    # src_features, tgt_features: (length of feature, #points)
-    voxel_size =2
-    source_down, source_fpfh = preprocess_point_cloud(source_pcd,voxel_size)
-    target_down, target_fpfh = preprocess_point_cloud(target_pcd, voxel_size)
-    source_data = np.asarray(source_down.points)
-    target_data = np.asarray(target_down.points)
-    src_features = np.asarray(source_fpfh.data)
-    tgt_features = np.asarray(target_fpfh.data)
+    """
     src_tree = o3d.geometry.KDTreeFlann(src_features)
     tgt_tree = o3d.geometry.KDTreeFlann(tgt_features)
     N_src = src_features.shape[1]
     N_tgt = tgt_features.shape[1]
-    dist_thres = 1 # a guess
-    #print(dir(src_tree))
-    # %%
-    # get matchers
-    #print(dir(src_tree))
-    #build two dicts to store features
+ 
     src_match_dict ={}
     tgt_match_dict ={}
     matchings = []
@@ -113,15 +98,71 @@ if __name__ == "__main__":
     for key in src_match_dict:
         for idx in src_match_dict[key]:
             if (key in tgt_match_dict[idx]):
+                #TODO: if there are any conditions should be satisfied.
                 #print("find a pair",key,", ",idx)
                 matchings.append((key,idx))
                 break
-    # get the best  R, T after iteration
+    return matchings
+
+
+def find_accociations(src_pts,R_init,t_init,threshold,tgttree):
+    """[summary]
+
+    Parameters
+    ----------
+    src_pts : numpy.array
+        The original data
+    R_init : 3*3 numpy.array
+        Previous Rotation matrix
+    t_init : 3*1 numpy.array
+        Previous translation vector
+    threshold : float
+        Decide if the pair is too far away
+    tgttree : Kdtree
+        A tree stores all the data information
+
+    Returns
+    -------
+    [type]
+        [description]
+    """
+    R_new = None
+    t_new = None
+    res =0
+    
+    
+    
+    return R_new,t_new, res
+
+        
 # %%
-    max_iter= 1
+if __name__ == "__main__":
+    
+    #load data
+    filename1 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+"0.bin"
+    filename2 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+"456.bin"
+    source_pcd = parse_bin_to_pcd(filename1)
+    target_pcd = parse_bin_to_pcd(filename2)
+    src_all_data = np.asarray(source_pcd.points).T
+    tgt_all_data = np.asarray(target_pcd.points).T
+    #test_pcd =io.read_point_cloud_bin(filename1)
+    #source_pcd.points
+    # %%
+     #VisualizePcd(source_pcd)
+    #VisualizePcd(target_pcd)
+    # src_features, tgt_features: (length of feature, #points)
+    voxel_size =2
+    source_down, source_fpfh = preprocess_point_cloud(source_pcd,voxel_size)
+    target_down, target_fpfh = preprocess_point_cloud(target_pcd, voxel_size)
+    source_data = np.asarray(source_down.points)
+    target_data = np.asarray(target_down.points)
+    src_features = np.asarray(source_fpfh.data)
+    tgt_features = np.asarray(target_fpfh.data)
+    matchings =getMatchesFromFeature(src_features, tgt_features)
+
+ # %%
     sample_num=9
-    pairs_num =(len(matchings))
-# %%
+    pairs_num =len(matchings)
     A_in_order =np.zeros((pairs_num,3))
     B_in_order =np.zeros((pairs_num,3))
     for i in range(pairs_num):
@@ -145,22 +186,8 @@ if __name__ == "__main__":
             tgt_idx = matchings[matches_set[j]][1]
             A_tmp[j]=source_data[src_idx]
             B_tmp[j]=target_data[tgt_idx]
-        # init registration
-        # 1R,t,residual =procrustes_transformation(A_tmp,B_tmp)
-        #print("R",R,"t",t)
-        A = A_tmp
-        B = B_tmp
-        N = A.shape[0]
-        
-        A_mean= np.mean(A,axis=0)
-        B_mean = np.mean(B,axis=0)
-        Ap = A - A_mean
-        Bp = B - B_mean
-        u, s, vt = np.linalg.svd(Bp.T@Ap)
-        #sR= vt.T@u.T
-        R= u@vt
 
-        t= np.expand_dims(B_mean - R@A_mean,1)
+        R,t = procrustes_transformation(A_tmp,B_tmp)
         new_A =(R@(A_in_order.T)+t).T
         diff = np.linalg.norm(new_A - B_in_order)
 
@@ -168,12 +195,30 @@ if __name__ == "__main__":
             best_R=R
             best_t=t
             min_res = diff
-    print("best R ",best_R)
-    print("best t ",best_t)
+    print("best init R ",best_R)
+    print("best init t ",best_t)
     # Observation, the t is always not correct after coarse optimization
             
  
 
+    # NOW, we have the best R.t for the initialization  matrix
+    # Do the icp
+    
+    # build a tree of ICP
+    #diaginal matrix
+    T_init = np.diag([1,1,1,1])
+    T[:3,:3] = best_R
+    T[:3,3] = best_t
+    tgt_data_tree = o3d.geometry.KDTreeFlann(tgt_all_data)
+    N= np.shape(src_all_data)[1]
+    tranformed_src_data = best_R@src_all_data 
+    # Try to match every point in the  stc
+
+
+
+
+    
  
 
 # %%
+
--- HW9/util.py
+++ HW9/util.py
@@ -26,7 +26,7 @@ def getOriginPcd(filename):
     point_cloud_o3d = point_cloud_pynt.to_instance("open3d", mesh=False)
     return point_cloud_o3d
 
-def getCuttedPcd(filename):
+def parse_bin_to_pcd(filename):
     """Extract pointcloud information from the raw .bin file
     and delete the norm information
 
@@ -46,10 +46,10 @@ def getCuttedPcd(filename):
     #data_np =  np.asarray(point_cloud_o3d.points)
     print(np.shape(data_np))
     all_data = (np.reshape(data_np, (int(data_np.shape[0]/6), 6)))
-    pcl_data = (all_data[:,:3])
-    print(np.shape(pcl_data))
-    point_cloud_o3d.points =  o3d.utility.Vector3dVector(pcl_data)
-    #point_cloud_o3d.normals =  o3d.utility.Vector3dVector((all_data[:,3:]))
+    #pcl_data = (all_data[:,:3])
+    #print(np.shape(pcl_data))
+    point_cloud_o3d.points =  o3d.utility.Vector3dVector(all_data[:,:3])
+    point_cloud_o3d.normals =  o3d.utility.Vector3dVector((all_data[:,3:]))
 
     return point_cloud_o3d
 
@@ -101,5 +101,7 @@ if __name__ =='__main__':
     filename1 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+"0.bin"
     result = getCuttedPcd(filename1)
     VisualizePcd(result)
-    
+    filename2 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+"1.bin"
+    result2 = getCuttedPcd(filename2)
+    VisualizePcd(result2)   
 # %%
