b08900b1495580e19d65c05624bf5cd3ca3a3eae
grobothero <hechangxin@gmail.com>
2021-06-04 09:40:52 +0200

    visualize the performance

--- HW9_new/register.py
+++ HW9_new/register.py
@@ -6,6 +6,7 @@ import numpy as np
 from pyntcloud import PyntCloud
 import operator
 import matplotlib.pyplot as plt
+import copy
 
 
 def parse_bin_to_pcd(filename):
@@ -34,9 +35,17 @@ def parse_bin_to_pcd(filename):
 
     return point_cloud_o3d
 
-def visualize_pcd(pcd):
-    o3d.visualization.draw_geometries([pcd])
-    
+def draw_registration_result(source, target, transformation=np.diag([1,1,1,1])):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp],
+                                      zoom=0.4559,
+                                      front=[0.6452, -0.3036, -0.7011],
+                                      lookat=[1.9892, 2.0208, 1.8945],
+                                      up=[-0.2779, -0.9482, 0.1556])
     
     # %%
 if __name__ == "__main__":
@@ -48,8 +57,12 @@ if __name__ == "__main__":
     filename1 = ("/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/{}.bin").format(idx1)
     filename2 = "/home/chahe/project/PointCloud3D/dataset/registration_dataset/point_clouds/"+idx2+".bin"
     src_pcd = parse_bin_to_pcd(filename1)
-    tgt_pcd = parse_bin_to_pcd(filename1)
+    tgt_pcd = parse_bin_to_pcd(filename2)
 
     #visualize_pcd(source_pcd)
     #could not visualize now!
+# %% step 2 denoise and downsample
+    voxel_size =2
+    pcd_down = src_pcd.voxel_down_sample(voxel_size)
+    draw_registration_result(src_pcd,tgt_pcd)
 # %%
